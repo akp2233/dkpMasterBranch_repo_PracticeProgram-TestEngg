eed50dfbcfbcf19f012b173bbe2076b83f7b85c3
dkp2233 <dkp2233@jira.com>
2026-01-16 10:18:15 +0530

    as test engg i created demo class and trying to push into master

---
+++ com.practice/src/test/java/front_End_Script/PracticeClass_TestEngg.java
@@ -0,0 +1,11 @@
+package front_End_Script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeClass_TestEngg {
+@Test
+public void demo() {
+	Reporter.log("created one class as engg and try to push into master branch");
+}
+}
